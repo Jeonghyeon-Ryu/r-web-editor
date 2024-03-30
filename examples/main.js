93fa6cdb636b417a8d18f6dec7890248dc5bd186
Jeonghyeon-Ryu <vxxv123@naver.com>
2024-03-30 22:38:22 +0900

    feat. 예제 파일 추가, index.js > RWebEditor Class 추가

---
+++ examples/main.js
@@ -0,0 +1,6 @@
+import { RWebEditor } from "../index.js"
+
+
+const baseElement = document.querySelector('#base-element')
+const webEditor = new RWebEditor(baseElement)
+
---
+++ index.js
@@ -0,0 +1,11 @@
+class RWebEditor {
+    constructor(el) {
+        console.log('생성자')
+        console.log(el)
+        this.el = el
+    }
+
+    
+}
+
+export { RWebEditor }
