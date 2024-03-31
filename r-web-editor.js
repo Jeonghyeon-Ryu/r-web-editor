258e598ffd82a6d7de99d914b720518672a6052f
Jeonghyeon-Ryu <vxxv123@naver.com>
2024-03-31 12:18:54 +0900

    feat. template 함수 추가.

--- r-web-editor.js
+++ r-web-editor.js
@@ -1,8 +1,25 @@
 class RWebEditor {
-    constructor(el) {
+    constructor(baseElement) {
         console.log('생성자')
-        console.log(el)
-        this.el = el
+        console.log(baseElement)
+        this.baseElement = baseElement
+        this.init()
+    }
+
+    init() {
+        const template = this.getTemplate()
+        for(let ele of template) {
+            this.baseElement.appendChild(ele)
+        }
+    }
+
+    getTemplate() {
+        const template = `<div>
+        
+        </div>`
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(template, 'text/html')
+        return [...doc.body.children];
     }
 
     
